5c569adeb63431062ee02c856f26b35680151107
jmo3300 <jmo3300@gmx.net>
2021-03-04 06:28:06 +0100

    api/greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [message, setMessage] = useState('Hello World from react1!...');
+
+  setTimeout(() =>
+    axios.get('/api/greeting')
+    .then(response => {
+      setMessage(response.data);
+    })
+    .catch(error => {
+      console.log(error)
+    }),1000
+
+  )
   return (
     <div className="App">
         <p>
-          Hello World!
+          {message}
         </p>
     </div>
   );
