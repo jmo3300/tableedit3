5c465df31913440eb9a41bd7be1414d6b972ed1f
jmo3300 <jmo3300@gmx.net>
2021-03-10 23:11:55 +0100

    minor modifications

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ export const App = () => {
   const [message, setMessage] = useState('Hello from react1 client! Trying to fetch greeting node1 server...');
 
   setTimeout(() =>
-    axios.get('/api/greeting')
+    // axios.get('/api/greeting')
+    axios.get('https://26h5l3u8kl.execute-api.eu-west-3.amazonaws.com/node1')
     .then(response => {
       setMessage(response.data);
     })
     .catch((error:AxiosError) => {
-      const message = `Cannot reach backend node1 server due to ${error.message}`
+      const message = `Cannot reach backend node1 server due to ${error.message}`;
+      console.log(`error: ${JSON.stringify(error)}`);
       setMessage(message);
     }),1000
 
